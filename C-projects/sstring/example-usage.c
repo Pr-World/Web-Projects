53884250cbc4d6c84977d3acc2fc468f39280c21
Prakash Niroula <82296809+Pr-World@users.noreply.github.com>
2021-11-16 08:29:32 +0545

    Update example-usage.c

--- C-projects/sstring/example-usage.c
+++ C-projects/sstring/example-usage.c
@@ -5,64 +5,57 @@ int main(){
 	//---------------- Simplicity Showcase -------------------
 	
 	sstring a;
-	a = "This is a sample string!";
-	printf("Sample: %s \n\n",a);
+	a = sstr.assign("This is a sample string!");
+	printf("Sample: %s \n\n",sstr.val(a));
 	
 	// ----------------- Functions ShowCase -------------------
 	
 	//all functions of sstring have error checking methods!
-	//create an error checking variable
-	sstrfuncerr var;
 	
 	
-	// -=-=- Sstrinput() -=-=-
-	// No other scanf() or gets() support sstring so, specially sstrinput() for sstrings!
-	printf("Sstrinput: ");
-	var = sstrinput();
+	// -=-=- sstr.getinput() -=-=-
+	// No other scanf() or gets() support sstring so, specially sstr.getinput() for sstrings!
+	printf("Enter a string: ");
+	a = sstr.getinput();
 	
 	//errcheck
-	if(var._err){
-		printf("\nAn error occured - Errcode: %d",var._err);
-		showerrs();
-	}else{
-		a = var._str;
-		printf("You entered: %s\n\n",a);
+	if(a._err!=_SSTRING_NO_ERR){
+		sstr.showerrs(a._err);
+	}elsse{
+		printf("You entered: %s\n\n",sstr.val(a));
 	}
 	
-	//-=-=- Sstrlen() -=-=-
+	//-=-=- sstr.len() -=-=-
 	// strlen() from string.h also can do this but added as a support for sstrings!
-	printf("Length of a: %d\n\n",sstrlen(a)); // Or printf("Length of a: %d\n\n",strlen(a)); <- from string.h
+	printf("Length of a: %d\n\n",sstr.len(a)); // Or printf("Length of a: %d\n\n",strlen(a)); <- from string.h
 	
-	//-=-=- Ssubstr() -=-=-
+	//-=-=- sstr.substr() -=-=-
 	//string from a pos=10, len=6 -> should return "sample"
 	
-	a = "This is a sample string";
-	var = ssubstr(a,10,6);
+	a = sstr.assign("This is a sample string");
+	a = sstr.substr(a,10,6);
 	
 	//errcheck
-	if(var._err){
-		printf("\nAn error occured - Errcode: %d",var._err);
-		showerrs();
+	if(a._err){
+		sstr.showerrs(a._err);
 	}else{
-		a = var._str;
-		printf("Substr: %s\n\n",a);
+		printf("Substr: %s\n\n",sstr.val(a));
 	}
 	
-	//-=-=- Sstrfind() -=-=-
+	//-=-=- sstr.find() -=-=-
 	
-	a = "Here first space is at 4th char!";
+	a = sstr.assign("Here first space is at 4th char!");
 	int check;
 	
 	//start from = 0, in string = a, what to find = " " space
 	//should return 4! >>"Here "<<!
-	check = sstrfind(0,a," ");
+	check = sstr.find(0,a," ");
 	
 	//errcheck
 	if((check<NO_ERR&&!(check==-1))||check>=sstrlen(a)){
-		printf("\nAn Error occured - Errcode: %d",check);
-		showerrs();
+		sstr.showerrs(check);
 	}else{
-		printf("First ' ' found at: %d in '%s'",check,a);
+		printf("First ' ' found at: %d in '%s'",check,sstr.val(a));
 	}
 
 }
